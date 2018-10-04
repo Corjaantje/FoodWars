824d6b20eadbfdb0eb41a3d19e27317af84a907a
Piet <pietburgmans_1@hotmail.com>
2018-10-04 11:20:06 +0200

    XMLReading, now filled out

--- TonicEngine/Sources/Storage/MyDocument.cpp
+++ TonicEngine/Sources/Storage/MyDocument.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "../../Headers/Storage/MyDocument.h"
+MyDocument::MyDocument(MyNode& root)
+        :_root(root)
+{
+
+}
+
+MyDocument::~MyDocument()
+{
+
+}
+
+MyNode MyDocument::GetRoot()
+{
+    return _root;
+}
+
+void MyDocument::AddToRoot(MyNode addition)
+{
+    _root.AddChild(addition);
+}

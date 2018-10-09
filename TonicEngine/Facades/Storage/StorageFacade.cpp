600652d8c63b5e9da72d36d379cd35ea6057ddeb
Piet <pietburgmans_1@hotmail.com>
2018-10-09 15:43:59 +0200

    Skeleton of the facade

--- TonicEngine/Facades/Storage/StorageFacade.cpp
+++ TonicEngine/Facades/Storage/StorageFacade.cpp
@@ -3,3 +3,65 @@
 //
 
 #include "../../Headers/Storage/StorageFacade.h"
+
+StorageFacade::StorageFacade() {
+
+}
+
+StorageFacade::~StorageFacade() {
+
+}
+
+// To allow switching between desired files.
+// Returns true if the desired file has been found. Prepares readableFile if true;
+bool StorageFacade::setTarget(std::string target) {
+    /* XMLDocument doc;
+    if (doc.LoadFile(target.c_str()))
+    {
+        //_readableFile = _reader.ReadXMLFile(doc);
+        return true;
+    }*/
+    return false;
+}
+
+// Reading
+void StorageFacade::getAudioComponents() {
+
+}
+
+void StorageFacade::getLevels() {
+
+}
+
+void StorageFacade::getTextures() {
+
+}
+
+void StorageFacade::getSavedGameData() {
+
+}
+
+void StorageFacade::getSavedUserData() {
+
+}
+
+void StorageFacade::getWeapons() {
+
+}
+
+// Writing
+void StorageFacade::saveCustomLevels() {
+
+}
+
+void StorageFacade::saveGameData() {
+
+}
+
+void StorageFacade::saveUserData() {
+
+}
+
+void StorageFacade::saveCustomWeapons() {
+
+}
--- TonicEngine/Headers/Storage/StorageFacade.h
+++ TonicEngine/Headers/Storage/StorageFacade.h
@@ -5,10 +5,36 @@
 #ifndef PROJECT_SWA_STORAGEFACADE_H
 #define PROJECT_SWA_STORAGEFACADE_H
 
+#include <string>
+
+#include "XMLReader.h"
+#include "XMLWriter.h"
 
 class StorageFacade {
+private:
 
+    XMLReader _reader;
+    XMLWriter _writer;
+public:
+    StorageFacade();
+    ~StorageFacade();
+
+    bool setTarget(std::string target);
+
+    // Placeholders for loading. Will end up using factories to return usable objects.
+    void getAudioComponents();
+    void getLevels();
+    void getTextures();
+    void getSavedGameData();
+    void getSavedUserData();
+    void getWeapons();
+
+
+    // Placeholders for writing.
+    void saveCustomLevels();
+    void saveGameData();
+    void saveUserData();
+    void saveCustomWeapons();
 };
-
 
 #endif //PROJECT_SWA_STORAGEFACADE_H

ddc12b34efb3478900897530c8ee052ed78fca68
Corjaantje <choskam@gmail.com>
2018-10-15 12:29:10 +0200

    Gameloop

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <ctime>
+#include <afxres.h>
 #include "TonicEngine/Headers/Visual/Window.h"
 #include "TonicEngine/Headers/Visual/VisualFacade.h"
 #include "TonicEngine/Headers/Input/InputObservable.h"
@@ -10,64 +12,28 @@
 int main(int argc, char** argv)
 {
     VisualFacade* visualFacade = new VisualFacade();
-    InputObservable* inputObservable = new InputObservable();
     visualFacade->setTitle("Food Wars");
-    //visualFacade->setResolution(1366, 768);
     visualFacade->setResolution(640, 480);
     visualFacade->disablefullscreen();
-    ShapeRectangle rectangle(200, 200, 0, 0, Colour(0, 255, 0, 255));
-    ShapeRectangle rectangle2(100, 100, 150, 150, Colour(0, 0, 255, 255));
-    ShapeRectangle rectangle3(50, 50, 225, 225, Colour(255, 0, 0, 255));
-
-    visualFacade->addRectangle(rectangle);
-    visualFacade->addRectangle(rectangle2);
-    visualFacade->addRectangle(rectangle3);
-
-//    ShapeSprite sprite(150, 150, 200, 0, "../grass.bmp");
-//    visualFacade->addSprite(sprite);
     visualFacade->openWindow();
 
-    EntityManager _entityManager;
-    int entity = _entityManager.createEntity(nullptr, 0);
-    std::cout << "Number of entities with drawable component: " << _entityManager.getAllEntitiesWithComponent<DrawableComponent>().size() << std::endl;
-    DrawableComponent d;
-    d.xPos = 10;
-    d.yPos = 11;
-    _entityManager.addComponentToEntity(entity, &d);
-    std::cout << "Number of entities with drawable component: " << _entityManager.getAllEntitiesWithComponent<DrawableComponent>().size() << std::endl;
+    //Config
+    clock_t startProgramTime = clock();
+    int maxMsProgramIsRunning = 10000; //Stop the program after 10 seconds
+    double frameRateCap = 60;
+    double amountOfUpdatesAllowedPerSecond = 1000.0 / frameRateCap; //= 16.666
+    //End of config
 
-    std::shared_ptr<DrawableComponent> dc = _entityManager.getComponentFromEntity<DrawableComponent>(entity);
-    dc->draw();
-    dc->xPos = 120;
-
-    //onderstaande code met test variabele werkt niet;
-    DrawableComponent test = *_entityManager.getComponentFromEntity<DrawableComponent>(entity);
-    test.xPos = 200;
-
-    for(auto const& iterator: _entityManager.getAllEntitiesWithComponent<DrawableComponent>()){
-        iterator.second->draw();
-    }
-
-    _entityManager.removeComponentFromEntity<DrawableComponent>(entity);
-    std::cout << "Number of entities with drawable component: " << _entityManager.getAllEntitiesWithComponent<DrawableComponent>().size() << std::endl;
-
-    _entityManager.addComponentToEntity(entity, new DrawableComponent(50,50));
-    int entity2 = _entityManager.createEntity();
-    _entityManager.addComponentToEntity(entity2, new DrawableComponent(99, 99));
-    for(auto const& iterator: _entityManager.getAllEntitiesWithComponent<DrawableComponent>()){
-        std::cout << iterator.first << ": ";
-        iterator.second->draw();
-    }
-
-    _entityManager.removeEntity(entity);
-    _entityManager.removeEntity(entity2);
-    _entityManager.removeEntity(entity2);
-    std::cout << "Number of entities with drawable component: " << _entityManager.getAllEntitiesWithComponent<DrawableComponent>().size() << std::endl;
-
-    while(!visualFacade->isWindowClosed()){
-        visualFacade->render();
+    clock_t timeLast = clock();
+    //Run the application only for MaxMSProgramIsRunning milliseconds.
+    while((clock() - startProgramTime / CLOCKS_PER_SEC * 1000 < maxMsProgramIsRunning) && !visualFacade->isWindowClosed()) {
         visualFacade->pollEvents();
-        //inputObservable->pollEvents();
+        double frameDelta = double (clock() - timeLast) / CLOCKS_PER_SEC * 1000.0;
+        double deltaTime = 1/frameDelta;
+        if(frameDelta > amountOfUpdatesAllowedPerSecond){
+            visualFacade->render();
+            timeLast = clock();
+        }
     }
 
     return 0;

51e68d9d96c5a7741b654fe697416e04104c8071
Piet <pietburgmans_1@hotmail.com>
2018-10-18 11:58:02 +0200

    Added default time per turn, removed comments

--- FoodWars/Headers/GameECS/Systems/TurnSystem.h
+++ FoodWars/Headers/GameECS/Systems/TurnSystem.h
@@ -8,66 +8,22 @@
 #include "IBaseSystem.h"
 #include "../Components/TurnComponent.h"
 
-//  Get components from the entity manager
-//  Turn
-//  Team
-//  Controllable?
-
-// Things important for the Turn System:
-//  Array of Players
-//      Players = Team Components
-//  Array of Characters which a Player controls (probably included in a Player?)
-//      Characters = Turn Components
-//  Starting Player (starting character random?)
-//  Turn max duration
-//
-//
-//
-
-//  RESPONSIBILITIES of the Turn System:
-//  Keeping track of a turn's duration
-//  Keeping the order of player's turns
-//      What about the character's order? Keep track of this too, I presume.
-//  Offer an indication that time's ticking
-//      Additional warning in last 5 seconds
-//
-//
-//
 class TurnSystem : public IBaseSystem {
 
 
 private:
-    int _timePerTurn = 30;
-//    double _remainingTime;
-
-//    int _timePassed;
-//    int timePassed();
+    int _timePerTurn;
+    int _defaultTimePerTurn = 30;
 
     EntityManager* _entityManager;
-//    std::vector<std::shared_ptr<TurnComponent>>* _turnComponents;
-    // Change everything related to this monstrosity ^
-    // So it just uses the crap from _entityManager
-    //std::map<int, std::shared_ptr<Comp>>
     std::map<int, std::shared_ptr<TurnComponent>> _turnComponents;
     int _currentTurn = 0;
     std::vector<int> _turnOrder;
 
-//    TurnComponent& _activeTurn;
-    // delta time
-    // 60 per sec, default
-    // Passing of time is based on the delta time. 60 times per second by default and then time _timePerTurn
-    // So for every 60 updates a second passes
-
-
-
 public:
-    // Treat 60 FPS as default
-//    TurnSystem();
     TurnSystem(EntityManager* entityManager);
-//    explicit TurnSystem(EntityManager* entityManager, int turnTime);
     ~TurnSystem() override;
 
-//    void setRelevantEntities(std::vector<std::shared_ptr<TurnComponent>>* turns);//Turn[] ent);
     void getRelevantEntities();
     void setRelevantEntities(std::map<int, std::shared_ptr<TurnComponent>>* turns);
     void setTurnTime(int turnTime);
@@ -78,17 +34,6 @@ public:
     // Uncertain if needed?
     // Would probably only be useful if we add a feature which allows you to switch to another character from your team.
     void switchCharacter(int charID);
-
-    /*
-        entities, get turn components
-	        (2 entities terug, integer & component)
-
-        Teams nog even negeren
-        Steeds maar 1 heeft "myturn = true"
-        Als beurt voorbij is, die op false en volgende true
-
-        void/bool/something EndTurn() of EndTurn(Entity end)
-     */
 };
 
 
--- FoodWars/Sources/GameECS/Systems/TurnSystem.cpp
+++ FoodWars/Sources/GameECS/Systems/TurnSystem.cpp
@@ -5,27 +5,16 @@
 #include "../../../Headers/GameECS/Systems/TurnSystem.h"
 
 //TurnSystem::TurnSystem() {
-////    _turnComponents = new std::map<int, std::shared_ptr<TurnComponent>>{};
-//    setTurnTime(30);
+//    setTurnTime(_defaultTimePerTurn);
 //}
 
 TurnSystem::TurnSystem(EntityManager* entityManager): _entityManager(entityManager){
-//    _turnComponents = new std::map<int, std::shared_ptr<TurnComponent>>{};
-    setTurnTime(30);
+    setTurnTime(_defaultTimePerTurn);
 }
-
-//TurnSystem::TurnSystem(EntityManager* entityManager, int turnTime): _entityManager(entityManager), _timePerTurn(turnTime) {
-//    //:_timePerTurn(turnTime*60)
-//}
 
 TurnSystem::~TurnSystem() {
 
 }
-
-//int TurnSystem::timePassed() {
-//    _timePerTurn--;
-//    return 0;
-//}
 
 void TurnSystem::getRelevantEntities() {
     _turnComponents = _entityManager->getAllEntitiesWithComponent<TurnComponent>();
@@ -35,31 +24,22 @@ void TurnSystem::getRelevantEntities() {
     {
         _turnOrder.push_back(x.first);
     }
-//    _activeTurn = *_turnComponents[_turnOrder[_currentTurn]];
+    setTurnTime(_defaultTimePerTurn);
 }
 
-//void TurnSystem::setRelevantEntities(std::vector<std::shared_ptr<TurnComponent>>* turns) {
 void TurnSystem::setRelevantEntities(std::map<int, std::shared_ptr<TurnComponent>>* turns) {
-//    _turnComponents->clear();
-//    if (!_turnComponents->empty())
-//    {
-//        _turnComponents->clear();
-//    }
-//    _turnComponents = turns;
-//    _turnComponents.assign(turns);
     _turnComponents = *turns;
-//    _currentTurn = turns->begin()->first;
     _currentTurn = 0;
     _turnOrder.clear();
     for (auto const& x : *turns)
     {
         _turnOrder.push_back(x.first);
     }
+    setTurnTime(_defaultTimePerTurn);
 }
 
 void TurnSystem::setTurnTime(int turnTime) {
     _timePerTurn = turnTime;
-//    _remainingTime = turnTime;
     if (!_turnOrder.empty())
     {
         _turnComponents[_turnOrder[_currentTurn]]->setRemainingTime(turnTime);
@@ -69,39 +49,22 @@ void TurnSystem::setTurnTime(int turnTime) {
 // Lower remaining time of 'active' TurnComponent
 // If remainingTime <= 0, end turn.
 void TurnSystem::update(double deltaTime) {
-//        _remainingTime -= deltaTime;
         _turnComponents[_turnOrder[_currentTurn]]->lowerRemainingTime(deltaTime);
-//        _activeTurn.lowerRemainingTime(deltaTime);
-//        _remainingTime = (_remainingTime > 0) ? _remainingTime - deltaTime : 30;
         if (_turnComponents[_turnOrder[_currentTurn]]->getRemainingTime() <= 0)
-//        if (_remainingTime <= 0)
         {
-//            _remainingTime = (float)_timePerTurn;
-            // TODO
-            // End turn of whomever's currently in charge
             endTurn();
         }
 }
 
 // End the 'active' TurnComponent's turn, start the next one.
 void TurnSystem::endTurn() {
-//    _turnComponents[_currentTurn]->switchTurn(false);
-////    _currentTurn++;
-//    _currentTurn = (_currentTurn < _turnComponents.size()) ? +1 : 0;
-//    _turnComponents[_currentTurn]->switchTurn(true);
-    // Change it to use a map, currently still thinking of a vector.
-    // How to get size of "participants"?
-    // Current participant? Next?
     _turnComponents[_turnOrder[_currentTurn]]->switchTurn(false);
 
     _currentTurn = (_currentTurn < _turnOrder.size()) ? +1 : 0;
-//    _activeTurn = *_turnComponents[_turnOrder[_currentTurn]];
 
     _turnComponents[_turnOrder[_currentTurn]]->switchTurn(true);
     _turnComponents[_turnOrder[_currentTurn]]->setRemainingTime((float)_timePerTurn);
-//    _turnComponents[_turnOrder[_currentTurn]]->switchTurn(false);
-//    _currentTurn = (_currentTurn < _turnOrder.size()) ? +1 : 0;
-//    _turnComponents[_turnOrder[_currentTurn]]->switchTurn(true);
+
 
 }
 

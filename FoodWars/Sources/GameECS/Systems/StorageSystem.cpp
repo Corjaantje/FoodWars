93195713eb68d5bdc49b5dfd245bb693dedc287d
Piet <pietburgmans_1@hotmail.com>
2018-11-07 15:02:34 +0100

    Neater/better loading WIP

--- FoodWars/Sources/GameECS/Systems/StorageSystem.cpp
+++ FoodWars/Sources/GameECS/Systems/StorageSystem.cpp
@@ -25,6 +25,45 @@ void StorageSystem::saveWorld() {
     myDoc = worldParser.ParseDrawableComponents(myDoc, _entityManager->getAllEntitiesWithComponent<DrawableComponent>());
 
 
+    // map[0] > <EntityID> as base
+    //            <DrawComponent>
+    //              DrawCompData
+    //            </DrawComponent>
+    //          </EntityID>
+    // map[1] >
+    // map[2] >
+    // map[3] >
+    // map[4] >
+    // map[5] >
+    // map[6] >
+    std::map<int, MyNode> allComponents { std::map<int, MyNode>() };
+//    MyNode drawNodes { "drawcomponent", &rootNode };
+
+    std::map<int, std::shared_ptr<DrawableComponent>> components {_entityManager->getAllEntitiesWithComponent<DrawableComponent>()};
+    // Can probably move this to a function with a <T> and pass a name for the newDraw?
+    // Would just need an array of the kinds of components.
+    for (auto const& drawComp : components)
+    {
+        // If there is a component with this ID
+        if (allComponents.count(drawComp.first))
+        {
+            // Gets the "base" of the structure for this EntityID
+            MyNode newDraw { "drawcomponent", allComponents[drawComp.first].FirstChildElement()};
+
+            allComponents[drawComp.first].FirstChildElement()->AddChild(newDraw);
+
+//            allComponents[drawComp.first].AddChild(newDraw);
+        }
+        else
+        {
+            MyNode newID { std::to_string(drawComp.first), &rootNode};
+
+            MyNode newDraw { "drawcomponent", &newID};
+
+            newID.AddChild(newDraw);
+            allComponents[drawComp.first] = newID;
+        }
+    }
 
     /*
      * "Iserializable" aanmaken in engine

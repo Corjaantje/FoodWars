38fdcbdf00e4d0e5212bd61d90ab7d503fc8f05f
Corjaantje <choskam@gmail.com>
2018-11-19 11:06:38 +0100

    Space bar Jump

--- FoodWars/Sources/GameECS/Systems/JumpSystem.cpp
+++ FoodWars/Sources/GameECS/Systems/JumpSystem.cpp
@@ -31,7 +31,7 @@ JumpSystem::JumpSystem(const std::shared_ptr<EntityManager> &entityManager,
 }
 
 void JumpSystem::update(std::shared_ptr<KeyEvent> event) {
-    if(event->getKeyEventType() == KeyEventType::Down && event->getKey() == KEY::KEY_W) {
+    if(event->getKeyEventType() == KeyEventType::Down && event->getKey() == KEY::KEY_SPACEBAR) {
         for(const auto &iterator: _entityManager->getAllEntitiesWithComponent<TurnComponent>()) {
             if(iterator.second->isMyTurn()){
                 if(!_entityManager->getComponentFromEntity<JumpComponent>(iterator.first)) {

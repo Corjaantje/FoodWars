9828d5ee85bc9a958d2391222b1c52c99a22a764
Unknown <smk.vanbeurden@student.avans.nl>
2018-11-19 12:42:18 +0100

    Minor bugfix to update highscore

--- FoodWars/Sources/StateMachine/HighscoreScreen.cpp
+++ FoodWars/Sources/StateMachine/HighscoreScreen.cpp
@@ -19,20 +19,20 @@ void HighscoreScreen::update(double deltaTime) {
 
     _renderList.clearLists();
 
-    _renderList.spriteList.emplace_back(ShapeSprite{1600, 900, 0, 0, "wallpaper3.png"});
+    _renderList._shapes[0].push_back(new ShapeSprite{1600, 900, 0, 0, "wallpaper3.png"});
 
     // Backbutton
     SpriteButton* backButton = new SpriteButton {*_inputFacade->getMouseEventObservable(), "backbutton.png", [c = _context]() {  c->setActiveScreen<MainMenuScreen>(); }, 100, 100, 9, 9, Colour{0,0,0,0}};
     backButton->addToRender(&_renderList);
     _buttons.push_back(backButton);
 
-    _renderList.textList.emplace_back(ShapeText(610, 130, "Highscore", 0, 400, 70, Colour(255, 120, 112, 0)));
-    _renderList.textList.emplace_back(ShapeText(560, 350, "Level 1: ", 0, 200, 85, Colour(255, 255, 255, 0)));
-    _renderList.textList.emplace_back(ShapeText(560, 510, "Level 2: ", 0, 200, 85, Colour(255, 255, 255, 0)));
-    _renderList.textList.emplace_back(ShapeText(560, 670, "Level 3: ", 0, 200, 85, Colour(255, 255, 255, 0)));
-    _renderList.textList.emplace_back(ShapeText(800, 350, _storageFacade->getHighscore(0) + " punten", 0, 250, 85, Colour(255, 255, 255, 0)));
-    _renderList.textList.emplace_back(ShapeText(800, 510, _storageFacade->getHighscore(1) + " punten", 0, 250, 85, Colour(255, 255, 255, 0)));
-    _renderList.textList.emplace_back(ShapeText(800, 670, _storageFacade->getHighscore(2) + " punten", 0, 250, 85, Colour(255, 255, 255, 0)));
+    _renderList._shapes[1].push_back(new ShapeText(610, 130, "Highscore", 0, 400, 70, Colour(255, 120, 112, 0)));
+    _renderList._shapes[1].push_back(new ShapeText(560, 350, "Level 1: ", 0, 200, 85, Colour(255, 255, 255, 0)));
+    _renderList._shapes[1].push_back(new ShapeText(560, 510, "Level 2: ", 0, 200, 85, Colour(255, 255, 255, 0)));
+    _renderList._shapes[1].push_back(new ShapeText(560, 670, "Level 3: ", 0, 200, 85, Colour(255, 255, 255, 0)));
+    _renderList._shapes[1].push_back(new ShapeText(800, 350, _storageFacade->getHighscore(0) + " punten", 0, 250, 85, Colour(255, 255, 255, 0)));
+    _renderList._shapes[1].push_back(new ShapeText(800, 510, _storageFacade->getHighscore(1) + " punten", 0, 250, 85, Colour(255, 255, 255, 0)));
+    _renderList._shapes[1].push_back(new ShapeText(800, 670, _storageFacade->getHighscore(2) + " punten", 0, 250, 85, Colour(255, 255, 255, 0)));
 
     visualFacade->render(_renderList);
     audioFacade->playMusic("menu");

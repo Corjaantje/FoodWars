87c7329a515f622f87c084e4c4703181da191017
Corjaantje <choskam@gmail.com>
2018-11-19 15:34:16 +0100

    Wallpaper in gamelevel

--- FoodWars/Headers/StateMachine/Misc/GameLevel.h
+++ FoodWars/Headers/StateMachine/Misc/GameLevel.h
@@ -12,14 +12,17 @@ public:
 private:
     EntityManager _entityManager;
     std::string _backgroundMusic;
+    std::string _backgroundWallpaper;
     std::vector<Coordinate> _spawnPoints;
 public:
     void setEntityManager(EntityManager entityManager);
     void setBackgroundMusic(std::string backgroundMusic);
+    void setBackgroundWallpaper(std::string backgroundWallpaper);
     void setSpawnPoints(std::vector<Coordinate> spawnPoints);
 
     EntityManager getEntityManager() const;
     std::string getBackgroundMusic() const;
+    std::string getBackgroundWallpaper() const;
     std::vector<Coordinate> getSpawnPoints() const;
 };
 

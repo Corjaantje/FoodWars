5d528673e4879649eed55eee6d659604646d6d72
Maarten <malebeek@avans.nl>
2018-11-20 23:47:26 +0100

    new lvl

--- FoodWars/Sources/LevelManager.cpp
+++ FoodWars/Sources/LevelManager.cpp
@@ -24,7 +24,7 @@ GameLevel* LevelManager::startLevel(int level) {
     // Load level
     StorageSystem* storage = new StorageSystem();
     _entityManager = EntityManager();
-    _gameLevel = storage->loadWorld(_entityManager, "Assets/Levels/Level12.xml");
+    _gameLevel = storage->loadWorld(_entityManager, "Assets/Levels/Level13.xml");
 
     if(_gameLevel){
         if(_gameLevel->getSpawnPoints().empty()){
